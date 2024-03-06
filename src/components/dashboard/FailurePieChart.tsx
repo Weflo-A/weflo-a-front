86f4df93a24d3f317031930b5a7d5599a5fe480e
이효원 <ymj07168@gmail.com>
2024-03-07 02:12:27 +0900

    [Feat/#55]: 라벨 직접 추가

--- src/components/dashboard/FailurePieChart.tsx
+++ src/components/dashboard/FailurePieChart.tsx
@@ -17,49 +17,42 @@ const FailurePieChart = () => {
   const options = {
     series: [45, 35, 15, 5],
     labels: ['Blade 고장', 'Motor 고장', 'ESC 고장', '조종기 고장'],
-    colors: [
-      colors.accent100,
-      colors.accentOpacity70,
-      colors.accentOpacity40,
-      colors.accentOpacity20,
-    ],
-    responsive: [
-      {
-        breakpoint: 480,
-        options: {
-          chart: {
-            width: 200,
-          },
-          legend: {
-            show: false,
-            position: 'bottom',
-          },
-        },
-      },
-    ],
     plotOptions: {
+      colors: [
+        colors.accent100,
+        colors.accentOpacity70,
+        colors.accentOpacity40,
+        colors.accentOpacity20,
+      ],
       legend: {
         show: false,
       },
       pie: {
         donut: {
-          size: '65%',
-          labels: {
+          size: '60%',
+          dataLabels: {
             show: true,
             name: {
               show: true,
               fontSize: '0.75rem',
               fontWeight: 700,
+              color: colors.basic700,
               offsetY: -5,
             },
             value: {
               show: true,
               fontSize: '1.125rem',
               fontWeight: 700,
+              color: colors.basic700,
               offsetY: 0,
               formatter: function (val: string) {
                 return val + '%'; // 밸류에 '%'를 추가합니다.
               },
+            },
+            totals: {
+              show: true,
+              showAlways: true,
+              label: 'Blade 고장',
             },
           },
         },
@@ -68,39 +61,48 @@ const FailurePieChart = () => {
   };
 
   return (
-    <div style={{ width: '100%' }}>
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '0.5rem',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
       <ReactApexChart
-        options={options}
+        options={options.plotOptions}
         series={options.series}
         type='donut'
         width='300'
       />
-      {/* <Stack direction='row'>
-        <Stack direction='column'>
+      <Stack direction='row' justifyContent='center' gap='0.5rem'>
+        <Stack direction='column' justifyContent='center' alignItems='center'>
           <LabelSquare color={colors.accent100} />
           <Typography fontSize='12px' color={colors.basic600}>
             Blade 고장
           </Typography>
         </Stack>
-        <Stack direction='column'>
+        <Stack direction='column' justifyContent='center' alignItems='center'>
           <LabelSquare color={colors.accentOpacity70} />
           <Typography fontSize='12px' color={colors.basic600}>
             ESC 고장
           </Typography>
         </Stack>
-        <Stack direction='column'>
+        <Stack direction='column' justifyContent='center' alignItems='center'>
           <LabelSquare color={colors.accentOpacity40} />
           <Typography fontSize='12px' color={colors.basic600}>
             Motor 고장
           </Typography>
         </Stack>
-        <Stack direction='column'>
+        <Stack direction='column' justifyContent='center' alignItems='center'>
           <LabelSquare color={colors.accentOpacity20} />
           <Typography fontSize='11px' color={colors.basic600}>
             조종기 고장
           </Typography>
         </Stack>
-      </Stack> */}
+      </Stack>
     </div>
   );
 };
